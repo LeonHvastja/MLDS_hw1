e3b60c0ce3c24aa0d339cd1f431e457b4fa9e934
LeonHvastja <leonhvastja@hotmail.com>
2023-03-05 20:34:44 +0100

    first working decision tree

--- hw_tree.py
+++ hw_tree.py
@@ -17,8 +17,8 @@ class Tree:
                  min_samples=2):
         self.rand = rand  # for replicability
         self.get_candidate_columns = get_candidate_columns  # needed for random forests
-        self.min_samples = min_samples
-
+        self.min_samples = min_samples 
+    
     def build(self, X: np.array, y: np.array):
         """
         Recusrively build a tree, stop recursion when a split has a child node with gini impurity 0 or
@@ -26,72 +26,81 @@ class Tree:
         """
         # should probably add some checks that the target vector entires are 1 and 0.
         
-        # just for robustness and testing
-        assert (len(X) == len(y)), "The input data and label vector are not of equal length" 
-        
         if (len(y) < self.min_samples): # we are in a leaf node
             return TreeNode(None, None, round(np.mean(y))) # make the majority class the prediction for this node
-        if (np.unique(y)[0] == 1): # check if we have a node with all ones
+        if (np.all(y == 1)): # check if we have a node with all ones
             return TreeNode(None, None, 1)
-        if (np.unique(y)[0] == 0):
+        if (np.all(y == 0)):
             return TreeNode(None, None, 0)
         
-        right_i, left_i, decision_rule = find_decision_rule(X, y)
+        decision_rule = self.find_decision_rule(X, y)
+        feature, split_value = decision_rule
+        
+        left_i = np.where(X[:,feature] < split_value)
+        right_i = np.where(X[:, feature] >= split_value)
         
         left_subtree = Tree()
         right_subtree = Tree()
         
-        
-        return TreeNode(left_subtree.build(X[left_i], y[left_i]),right_subtree.build(X[right_i], y[right_i]), decision_rule) 
-
-
-    def find_decision_rule(X, y):
+        return TreeNode(left_subtree.build(X[left_i], y[left_i]),
+                        right_subtree.build(X[right_i], y[right_i]), 
+                        decision_rule) 
+    
+    
+    def find_decision_rule(self, X, y):
         """
         Input: X - data, y - labels
         Output: A tuple (left, right, decision_rule), left indicies, right indicies and rule. Rule itself is a tuple
         of the index of the feature to split on and the value of where to split.)
         """
-
         decision_rule = None
-        best_info_gain = 0 
-        
-        for i, feature in enumerate(X.T): # we iterate over rows of the transpose so it's actually over columns
-            # TODO: better implementation, this just uses the mean to split
-            split_value = np.mean(feature) # this is just a number of where to split
-            left = np.where(feature < split_value)[0]
-            right = np.where(feature >= split_value)[0]
-            
-            
-            current_info_gain = information_gain(y, left, right)
-            if (current_info_gain > best_info_gain):
-                best_info_gain = current_info_gain
-                decision_rule = (i, split_value)
-        
-        return (left, right, decision_rule) 
+        best_info_gain = 0
+
+        for feature in self.get_candidate_columns(X, self.rand):
+            values = X[:, feature]
+            sorted_indices = np.argsort(values)
+            sorted_values = values[sorted_indices]
+            for i in range(len(sorted_values) - 1):
+                current_info_gain = self.information_gain(y[sorted_indices], np.arange(0,i+1), np.arange(i+1, len(values)))
+                
+                if(current_info_gain > best_info_gain):
+                    split_value = self.midpoint(i, sorted_values)
+                    best_info_gain = current_info_gain
+                    decision_rule = (feature, split_value)
+
+        return decision_rule
+                
+    def midpoint(self, index, y):
+        """Finds the average value of entires at index i and i+1 in a presumably sorted array."""
+        return (y[index] + y[index + 1])/2
     
-    def gini_impurity(y):
+    def information_gain(self, y , left_partition_indicies, right_partition_indicies):
+        """
+        Input: Takes an array of labels and the indicies of which belong to the lefr and right partition.
+        Output: Returns information gain for this particular split.
+        """
+        n_left = len(left_partition_indicies)
+        n_right = len(right_partition_indicies)
+        n = n_left + n_right
+
+        l_weight = n_left/n
+        r_weight = n_right/n
+
+        inf_gain = (self.gini_impurity(y) 
+                    - self.gini_impurity(y[left_partition_indicies])*l_weight 
+                    - self.gini_impurity(y[right_partition_indicies])*r_weight)
+
+        return inf_gain
+
+    def gini_impurity(self, y):
         """
         We can use this simplified version because we are solving a strictly binary classification problem, 
         assume y is a numpy array with values of 0 or 1.
         """
-        
+
         label_one_probability = sum(y)/len(y)
-        
+
         return 1 - ((label_one_probability)**2 + (1-label_one_probability)**2)
-    
-    def information_gain(y , left_partition_indicies, right_partition_indicies):
-        n_left = len(left_partition_indicies)
-        n_right = len(right_partition_indicies)
-        n = n_left + n_right
-        
-        l_weight = n_left/n
-        r_weight = n_right/n
-        
-        inf_gain = (gini_impurity(y[np.concatenate((left_partition_indicies,right_partition_indicies))]) 
-                    - gini_impurity(y[left_partition_indicies])*l_weight 
-                    - gini_impurity(y[right_partition_indicies])*r_weight)
-        
-        return inf_gain
 
 
 class TreeNode:
